f2810f8c528f57ae373d9e5faed0df25fc08def0
h4ck3r@parrot <rajeevgoutham1@protonmail.com>
2025-09-30 02:20:52 +0530

    fix: Updated CORS middleware for '*'

--- src/utils/middlewares/cors.rs
+++ src/utils/middlewares/cors.rs
@@ -5,7 +5,7 @@ pub fn cors_middleware() -> Cors {
     let config = &get_config().server;
     let mut cors = Cors::default().allow_any_header().allow_any_method();
     if config.cors_url.len() == 1 && config.cors_url[0] == "*" && !config.production {
-        cors = cors.allow_any_origin();
+        cors = cors.allow_any_origin().send_wildcard();
     } else {
         for origin in &config.cors_url {
             cors = cors.allowed_origin(origin);
