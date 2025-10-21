58fe33bb294fa88505cad1e2daa392fe091d39b5
h4ck3r@parrot <rajeevgoutham1@protonmail.com>
2025-10-21 11:03:25 +0530

    chore: Updated events license

--- src/utils/events/event.rs
+++ src/utils/events/event.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 Rodan
+// Copyright (c) 2025 Intraware
 // Licensed under the MIT License
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
